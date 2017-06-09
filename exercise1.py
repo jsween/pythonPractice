e66cb33ff22f522edaffd0f0223058b8c3787681
Jon Sweeney <jonsweeney26@gmail.com>
2017-06-09 07:05:08 -0700

    add logic to print message favNum times

--- exercise1.py
+++ exercise1.py
@@ -2,14 +2,27 @@ CURRENT_YEAR = 2017
 
 def get_user_age():
     isInteger = False
-    while (isInteger == False):
-        userAge = input("Please Enter Your Age: \n")
+    while (not isInteger):
+        userAge = input("Please Enter Your Age: ")
         isInteger = represents_int(userAge)
         if (isInteger == True):
             return int(userAge)
         else:
             print("Please insert a valid age.")
 
+def get_fav_num():
+    isInteger = False
+    while (not isInteger):
+        favNum = input("Please Enter Your Favorite Number: ")
+        isInteger = represents_int(favNum)
+        if(isInteger == True):
+            return int(favNum)
+        else:
+            print(favNum + " is not authorized to be a favorite number!!!\nPlease try again...")
+
+def print_output_fav_num_times(numTimes):
+    for x in range(numTimes):
+        print(create_response(yearWhen100))
 
 def represents_int(input):
     try:
@@ -35,8 +48,9 @@ def create_response(yearWhenTurn100):
         return "Congratulations " + username + "!!! This is the year you turn 100!!!"
 
 
-username = input("Please Enter Your First Name: \n").capitalize()
+username = input("Please Enter Your First Name: ").capitalize()
 userAge = get_user_age()
+favNum = get_fav_num()
 
 yearWhen100 = find_year_when_100(userAge)
-print(create_response(yearWhen100))
+print_output_fav_num_times(favNum)
