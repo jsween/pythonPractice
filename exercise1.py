bfa5bfb396f284500ddcb6a620278085547ec94d
Jon Sweeney <jonsweeney26@gmail.com>
2017-06-09 06:10:20 -0700

    add logic for user over 100

--- exercise1.py
+++ exercise1.py
@@ -2,13 +2,13 @@ CURRENT_YEAR = 2017
 
 def find_year_when_100(age):
     if age > 100:
-        return -1
+        return CURRENT_YEAR - (age - 100)
     else:
         return (100 - age) + CURRENT_YEAR
 
 def create_response(yearWhenTurn100):
-    if yearWhenTurn100 == "-1":
-        return "You're Old!"
+    if yearWhenTurn100 > "0":
+        return "You're old " + username + "! In " + yearWhenTurn100 + ", you turned 100!!!" 
     else:
         return "Wow " + username + ", in the year " + yearWhenTurn100 + ", you'll be 100!!!"
 
