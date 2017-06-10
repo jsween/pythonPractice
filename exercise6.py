a3612c6584cf7515b9f82364c144546e6103b905
Jon Sweeney <jonsweeney26@gmail.com>
2017-06-10 12:58:00 -0700

    add logic to set input to lowercase

--- exercise6.py
+++ exercise6.py
@@ -1,9 +1,24 @@
-
-
-word = input("Enter a word: ")
+"""
+# This is option 1 without loop
+word = input("Enter a word: ").lower()
 reverse = word[::-1]
 
 if word != reverse:
-    print(word + " and " + reverse + " are not palindromes")
+    print(word + " is not a palindrome")
 else:
-    print(word + " and " + reverse + " are palindromes!!!")
+    print(word + " is a palindrome!!!")
+"""
+# This is option 2 with a loop
+def reverse_word(word):
+    reverse_word = ""
+    for x in range(len(word)):
+        reverse_word += word[len(word) - 1 - x]
+    return reverse_word
+
+word = input("Enter a word: ").lower()
+reverse_word = reverse_word(word)
+
+if word != reverse_word:
+    print(word + " is not a palindrome")
+else:
+    print(word + " is a palindrome!!!")
