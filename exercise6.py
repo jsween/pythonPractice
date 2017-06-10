4fb5ce26f0cc000b687450a89e5415a57916ca6c
Jon Sweeney <jonsweeney26@gmail.com>
2017-06-10 11:50:53 -0700

    add palindrome ex6

--- exercise6.py
+++ exercise6.py
@@ -1,3 +1,9 @@
 
 
 word = input("Enter a word: ")
+reverse = word[::-1]
+
+if word != reverse:
+    print(word + " and " + reverse + " are not palindromes")
+else:
+    print(word + " and " + reverse + " are palindromes!!!")
